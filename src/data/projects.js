1d01e58f86106990fa9dda3b70d0676418f72432
zprimus <zachary_primus@yahoo.com>
2021-05-06 11:33:29 -0400

    updated data and styling

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,9 +17,9 @@ const projects = [
   {
     id: 'portfolio',
     name: 'Portfolio',
-    overview: '',
-    desc1: 'desc',
-    desc2: 'desc',
+    overview: 'An overview of my projects and experiences.',
+    desc1: 'This project was created solely by me for me.',
+    desc2: 'I created templates for projects and locations so that future additions can be auto-populated from filling out a few strings.',
     skills: [
       'CSS',
       'JavaScript',
@@ -46,8 +46,17 @@ const projects = [
     id: 'p41-ephenry',
     name: 'P4.1 EP Henry',
     overview: "Large-scale industrial system for curing concrete pavers that consume CO2.",
-    desc1: "Project developed on the Electrical team for Solidia Technologies. It was installed at our customer's facility, EP Henry.",
-    desc2: "desc",
+    desc1: "This project was developed on the Electrical team for Solidia Technologies. " +
+      "It was installed at one of our customers' facilities, EP Henry. " +
+      'The long-term goal was the production of "green" concrete pavers that consume CO2 from the environment.',
+    desc2: "I was a lead on this project for the Electrical team. " +
+    "I designed the electrical drawings (power distribution, network topology, control architecture, etc.) using AutoCAD. " +
+    "After the design process, we installed the fabricated panels and wired the entire project of 100+ devices. " +
+    "Using ladder logic, we programmed the controls for the various closed-loop systems. " +
+    "I also configured the database and designed the HMI (Human Machine Interface) for the operator to easily monitor and control the systems. " +
+    "Once the project was done, we spent weeks monitoring and making adjustments for the different runs. " +
+    "The project was then handed over to the customer."
+    ,
     skills: [
       'AutoCAD',
       'Ignition',
@@ -85,9 +94,14 @@ const projects = [
   {
     id: 'p321-bramptonbrick',
     name: 'P3.2.1 Brampton Brick',
-    overview: 'Industrial prototype for curing concrete pavers that consume CO2 for testing purposes.',
-    desc1: 'desc',
-    desc2: 'desc',
+    overview: 'Industrial prototype for curing "green" concrete pavers that consume CO2.',
+    desc1: "This project was developed on the Electrical team for Solidia Technologies. " +
+    "It was installed at one of our customers' facilities, Brampton Brick. " +
+    'The goal was the testing of "green" concrete pavers that consume CO2 from the environment.',
+    desc2: 'My involvment consisted of creating the electrical drawings. ' +
+    'I helped with the install of electrical equipment at the facility. ' +
+    'I also developed the HMI (Human Machine Interface) and the ladder logic for controls.' +
+    'For the many runs that went on for approximately 2 years, I provided onsite and remote support.',
     skills: [
       'FactoryTalk ME',
       'Ignition',

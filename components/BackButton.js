553e2e03b2a48d9fb4efe06338321f2d337e866c
zprimus <zachary_primus@yahoo.com>
2022-11-09 08:29:06 -0500

    added animation

--- components/BackButton.js
+++ components/BackButton.js
@@ -5,19 +5,19 @@ import Link from 'next/link';
 const BackButton = (props) => {
     return (
         <Link href={`${props.url}`} style={{ textDecoration: 'none' }}>
-            <button className="BackButton">
+            <div className="BackButton">
                 <svg viewBox="0 0 100 80" width="40" height="40">
                     <g transform="translate(0 35) rotate(-45)">
-                        <rect id="arrow" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
+                        <rect id="arrow1" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
                     </g>
                     <g transform="translate(0 35) rotate(0)">
                         <rect id="straight" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
                     </g>
                     <g transform="translate(10 35) rotate(45)">
-                        <rect id="arrow" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
+                        <rect id="arrow2" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
                     </g>
                 </svg>
-            </button>
+            </div>
         </Link>
     );
   }
--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,28 +1,42 @@
 // dependencies
 import React from 'react';
 
-const MenuButton = (props) => {
+const MenuButton = ({showMenu, handleClickMenu}) => {
     return (
-        props.showMenu ? (
-            <button className="MenuButton" onMouseDown={() => props.handleClickMenu()}>
-                <svg viewBox="0 0 100 80" width="40" height="40">
-                    <g transform="translate(10 70) rotate(-45)">
-                        <rect id="MenuButton-line1" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
-                    </g>
-                    <g transform="translate(20 0) rotate(45)">
-                        <rect id="MenuButton-line1" rx="15"  stroke="#66FCF1" strokeWidth="5"></rect>
-                    </g>
-                </svg>
-            </button>
-        ) : (
-            <button className="MenuButton" onMouseDown={() => props.handleClickMenu()}>
-                <svg viewBox="0 0 100 80" width="40" height="40">
-                    <rect id="MenuButton-line1" rx="15" stroke="#66FCF1" strokeWidth="5"></rect>
-                    <rect id="MenuButton-line1" rx="15" y="30" stroke="#66FCF1" strokeWidth="5"></rect>
-                    <rect id="MenuButton-line1" rx="15" y="60" stroke="#66FCF1" strokeWidth="5"></rect>
-                </svg>
-            </button>
-        )
+        <div 
+            className="MenuButton" 
+            onMouseUp={() => handleClickMenu()}
+        >
+            <svg 
+                viewBox="0 0 100 80" 
+                width="40" 
+                height="40"
+            >
+                <rect 
+                    className={showMenu ? "closeMenu" : "openMenu"}
+                    id="MenuButton-line1" 
+                    rx="15" 
+                    stroke="#66FCF1" 
+                    strokeWidth="5"
+                ></rect>
+                <rect 
+                    className={showMenu ? "closeMenu" : "openMenu"}
+                    id="MenuButton-line2" 
+                    rx="15" 
+                    y="30" 
+                    stroke="#66FCF1" 
+                    strokeWidth="5"
+                ></rect>
+                <rect 
+                    className={showMenu ? "closeMenu" : "openMenu"}
+                    id="MenuButton-line3" 
+                    rx="15" 
+                    y="60" 
+                    stroke="#66FCF1" 
+                    strokeWidth="5"
+                ></rect>
+            </svg>
+        </div>
     );
   }
 
